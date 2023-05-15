12947dc9e1182bdb4fe6710843ed4598f297f7c6
Khiem Le <theinnocentman1@gmail.com>
2023-05-15 16:19:16 +0700

    Update NavBar item hover anim

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,8 +1,10 @@
 import headerNavLinks from '@/data/headerNavLinks'
 import Logo from '@/data/logo.svg'
 import siteMetadata from '@/data/siteMetadata'
+import classNames from 'classnames'
 import { AnimatePresence, motion } from 'framer-motion'
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 import Footer from './Footer'
 import Link from './Link'
 import MobileNav from './MobileNav'
@@ -11,6 +13,8 @@ import ThemeSwitch from './ThemeSwitch'
 
 const LayoutWrapper = ({ children }) => {
   const router = useRouter()
+
+  const [hoveredItem, setHoveredItem] = useState(null)
 
   return (
     <SectionContainer>
@@ -32,15 +36,43 @@ const LayoutWrapper = ({ children }) => {
               </div>
             </Link>
           </div>
-          <div className="flex items-center text-base leading-5">
+          <div className="flex items-center justify-center text-base leading-5">
             <div className="hidden sm:block">
               {headerNavLinks.map((link) => (
-                <Link
-                  key={link.title}
-                  href={link.href}
-                  className="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4"
-                >
-                  {link.title}
+                <Link key={link.title} href={link.href}>
+                  <motion.span
+                    onMouseEnter={() => setHoveredItem(link.title)}
+                    onMouseLeave={() => setHoveredItem(null)}
+                    className={classNames(
+                      'relative cursor-pointer rounded-md p-1 duration-200 ease-in-out md:p-4',
+                      {
+                        'font-medium': link.href != router.asPath,
+                        'font-bold ': link.href == router.asPath,
+                      }
+                    )}
+                  >
+                    <AnimatePresence>
+                      {hoveredItem == link.title && (
+                        <motion.span
+                          layoutId="hoverItem"
+                          className="absolute inset-0 z-0 h-full w-full rounded-md bg-primary-500 bg-opacity-20"
+                          initial={false}
+                          animate={{
+                            opacity: 1.0,
+                            scale: 1.0,
+                          }}
+                          exit={{
+                            opacity: 0.3,
+                            scale: 0.3,
+                          }}
+                          transition={{
+                            scale: { duration: 0.2, ease: 'easeIn' },
+                          }}
+                        />
+                      )}
+                    </AnimatePresence>
+                    <span className="relative z-10">{link.title}</span>
+                  </motion.span>
                 </Link>
               ))}
             </div>
